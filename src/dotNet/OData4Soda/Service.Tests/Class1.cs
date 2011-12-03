4104372c72c517551cb4e76f3ebd2e1a056a9309
Arlo Belshee <arlo.belshee@live.com>
2011-12-03 13:46:27 -0800

    Added some approval test stuff, then took it back out.

--- src/dotNet/OData4Soda/Service.Tests/Class1.cs
+++ src/dotNet/OData4Soda/Service.Tests/Class1.cs
@@ -1,12 +1,19 @@
-﻿using NUnit.Framework;
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
 
 namespace Service.Tests
 {
-	[TestFixture]
+	[TestFixture, UseReporter(typeof(DiffReporter))]
 	public class Class1
 	{
 		[Test]
 		public void AlwaysPasses()
+		{
+		}
+
+		[Test]
+		public void ConstantlySeekingApproval()
 		{
 		}
 	}
